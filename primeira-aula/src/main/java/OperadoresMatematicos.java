32cd421869caf0866cca9e5dd14f552e0ae344e9
GustavoFernandesSP <>
2024-02-15 19:00:52 -0300

    Foi ajustado a formatação do texto e adicionado quebras de linha.

--- primeira-aula/src/main/java/OperadoresMatematicos.java
+++ primeira-aula/src/main/java/OperadoresMatematicos.java
@@ -9,19 +9,21 @@ public class OperadoresMatematicos {
         Double numero01 = 42.0;
         Double numero02 = 2.0;
 
-        System.out.println("Soma \n" + (numero01 + numero02) + "\n");
+        System.out.println("\n");
+
+        System.out.println("Soma: \n" + (numero01 + numero02) + "\n");
         // usa-se \n para quebra de linha e ele sempre tem que estar dentro das aspas "\n" como no exemplo a cima.
 
-        System.out.println("Multiplicação " + (numero01 * numero02));
+        System.out.println("Multiplicação: \n" + (numero01 * numero02)+ "\n");
 
-        System.out.println("Subtração " + (numero01 - numero02));
+        System.out.println("Subtração: \n" + (numero01 - numero02)+ "\n");
 
-        System.out.println("Divisão " + (numero01 / numero02));
+        System.out.println("Divisão: \n" + (numero01 / numero02)+ "\n");
 
 
         Double resultadoPotencia = Math.pow(numero01, numero02);
 
-        System.out.println("Resultado da Potência: " + resultadoPotencia);
+        System.out.println("Resultado da Potência: \n" + resultadoPotencia + "\n");
     }
 
 }
