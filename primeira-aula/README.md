da6c4806e4d12c4479ea7a6d775fc7f12467c47c
GustavoFernandesSP <>
2024-02-15 17:38:32 -0300

    First commit

---
+++ primeira-aula/README.md
@@ -0,0 +1,6 @@
+### Hello, welcome to the first java class 🤙
+
+- 👋 Olá pode me chamar de Gusta.
+- 🎓 SPtech School.
+
+<h1> Gustavo Fernandes</h1>
---
+++ primeira-aula/src/main/java/RevisaoAulaPassada.java
@@ -0,0 +1,39 @@
+public class RevisaoAulaPassada {
+
+    public static void main(String[] args){
+
+        // tipos que mais usaremos
+
+        String nome = "Gustavo";
+
+        Integer numeroSorteio = 42;
+
+        Double altura = 1.67;
+
+        Boolean verdadeiro = false;
+
+        /////////////////////////////////////////////////
+
+        // Tips Wrapper e tips primitivos
+        // Tips Wrapper são classes, e tem métodos que ajudam :)
+        // Tips primitivos não são classes e nem métodos :(
+
+        // Tipos Wrapper existem métodos já tipos primitivos não
+        // tem métodos, os tipos primitivos mais utilizado no for como exemplo
+        // no i, no geral sempre escrever no tip Wrapper
+
+        Integer  numeroTipoWrapper = 10;
+
+        int numeroTipoPrimitivo = 10;
+
+        // Atalho  *sout*
+        System.out.println(
+                "meu nome é " + nome +
+                " meu número sorteado é " + numeroSorteio +
+                " e minha altura é " + altura);
+
+        
+
+    }
+
+}
